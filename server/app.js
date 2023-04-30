0bfe7602999733269347efeb2c2f4686bf1c30f0
Veniamin Polienko <kasaklalita@mail.ru>
2023-04-30 16:57:06 +0300

    Author type

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const schema = require("./schema/schema.js");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP.createHandler({ schema }));
+app.use("/graphql", graphqlHTTP.createHandler({ schema, graphiql: true }));
 
 app.listen(4000, () => {
   console.log("now listening for requests on port 4000");
