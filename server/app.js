907809dd33c965f9d51a4f908c9290641ba09815
Veniamin Polienko <kasaklalita@mail.ru>
2023-04-30 13:48:52 +0300

    The resolve function

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const graphqlHTTP = require("graphql-http/lib/use/express");
+const schema = require("./schema/schema.js");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP.createHandler({}));
+app.use("/graphql", graphqlHTTP.createHandler({ schema }));
 
 app.listen(4000, () => {
   console.log("now listening for requests on port 4000");
