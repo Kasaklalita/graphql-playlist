483335bdce7a01a8751c7c3b31ee8914816ff7ee
Veniamin Polienko <kasaklalita@mail.ru>
2023-04-30 13:21:01 +0300

    Setting up graphql

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const graphqlHTTP = require("express-qraphql");
+const graphqlHTTP = require("graphql-http/lib/use/express");
 
 const app = express();
+
+app.use("/graphql", graphqlHTTP.createHandler({}));
 
 app.listen(4000, () => {
   console.log("now listening for requests on port 4000");
